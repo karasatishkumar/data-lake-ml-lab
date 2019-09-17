0a71ef421393946e7ca58efe4e197cdb17033c2d
Kara Satish Kumar <karasatishkumar@gmail.com>
2019-09-17 11:35:25 +0530

    working spark example for word count

--- sample-spark-wordcount/src/main/java/org/satish/spark/WordCounter.java
+++ sample-spark-wordcount/src/main/java/org/satish/spark/WordCounter.java
@@ -1,39 +1,33 @@
 package org.satish.spark;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.regex.Pattern;
 
 public class WordCounter
 {
     private static final Pattern SPACE = Pattern.compile(" ");
+
     public static void main (String[] args)
     {
         if (args.length < 1) {
             System.err.println("Usage: JavaWordCount <file>");
             System.exit(1);
         }
-        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
-        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        JavaRDD<String> lines = ctx.textFile(args[0], 1);
-
-        JavaRDD<String> words
-            = lines.flatMap(s -> (List<String>)Arrays.asList(SPACE.split(s)).iterator());
-        JavaPairRDD<String, Integer> ones
-            = words.mapToPair(word -> new Tuple2<>(word, 1));
-        JavaPairRDD<String, Integer> counts
-            = ones.reduceByKey((Integer i1, Integer i2) -> i1 + i2);
-
-        List<Tuple2<String, Integer>> output = counts.collect();
-        for (Tuple2<?, ?> tuple : output) {
-            System.out.println(tuple._1() + ": " + tuple._2());
-        }
-        ctx.stop();
+        SparkSession spark = SparkSession.builder().appName(WordCounter.class.getSimpleName()).master(
+            "local").getOrCreate();
+        Dataset<String> df = spark.read().textFile(args[0]);
+        FlatMapFunction<String, String> wordsExtractFunction = (line) -> {
+            String lowerCaseLine = line.toLowerCase();
+            return Arrays.asList(lowerCaseLine.split(" ")).iterator();
+        };
+        Dataset<String> words = df.flatMap(wordsExtractFunction, Encoders.STRING());
+        Dataset<Row> countDS = words.groupBy("value").count();
+        countDS.show();
     }
 }
